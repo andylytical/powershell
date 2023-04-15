347f4793f64b3f854703bf4705e976e6e9062a09
Andy Loftus <aloftus@gmail.com>
2023-04-15 18:36:29 +0000

    start "Work" links only on workdays

--- custom_startup.ps1
+++ custom_startup.ps1
@@ -1,47 +1,66 @@
-﻿
-# SEE ALSO
-# (Get-WmiObject Win32_OperatingSystem).OSArchitecture
-# Returns: String ( "32-bit" OR "64-bit" )
-# Reference: https://blogs.msdn.microsoft.com/koteshb/2010/02/12/powershell-how-to-find-details-of-operating-system/
-
 $STARTUP_DIR = "$env:USERPROFILE\Onedrive\Startup"
 $PAUSE = 5
-
-#start-sleep -s $PAUSE
+$DRYRUN = $false
+#$DRYRUN = $true
 
 $sh = New-Object -ComObject WScript.Shell
 
 function Invoke-ShortcutIfExists {
-    param (
-        $raw_path
-    )
+  param (
+    $raw_path
+  )
 
-    write-host ""
-    write-host "Invoke-ShortcutIfExists"
-    write-host $raw_path
-    $shortcut = $sh.CreateShortcut($raw_path)
-    $tgt = $shortcut.TargetPath
-    write-host $tgt
-    $exists = Test-Path -Path $tgt
-    write-host "'$raw_path' -> '$tgt' ... exists? '$exists'"
-    if ( $exists ) { 
-        invoke-item "$raw_path"
-        Start-sleep -s $PAUSE
-    }
+  write-host "-----"
+  write-host "Invoke-ShortcutIfExists"
+  write-host $raw_path
+  $shortcut = $sh.CreateShortcut($raw_path)
+  $tgt = $shortcut.TargetPath
+  write-host $tgt
+  $exists = Test-Path -Path $tgt
+  write-host "'$raw_path' -> '$tgt' ... exists? '$exists'"
+  if ( $exists -And -Not $DRYRUN ) { 
+    invoke-item "$raw_path"
+    Start-sleep -s $PAUSE
+  }
+}
+
+function IsBusinessHours {
+  $now = Get-Date
+  $dayOfWeek = $now.DayOfWeek
+  $hourOfDay = $now.Hour
+
+  if ($dayOfWeek.ToString().StartsWith("S")) {
+    return $false
+  }
+
+  if ($hourOfDay -lt 7 -or $hourOfDay -ge 17) {
+    return $false
+  }
+
+  return $true
 }
 
 # Run all architecture agnostic progs
 gci "$STARTUP_DIR/*" -Filter *.lnk -Exclude 00* | foreach-object {
-    Invoke-ShortcutIfExists $_
+  Invoke-ShortcutIfExists $_
 }
 
 # Run architecture specific progs
+# ...
+# see also (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+# Returns: String ( "32-bit" OR "64-bit" )
+# https://blogs.msdn.microsoft.com/koteshb/2010/02/12/powershell-how-to-find-details-of-operating-system/
 $arch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture.Substring(0,2)
 $archdir = "$STARTUP_DIR\$arch"
 gci "$archdir" -Filter *.lnk | foreach { 
-    write-host "RAW: $_"
-    write-host "WITHDIR: $archdir\$_"
-    Invoke-ShortcutIfExists $archdir\$_
-    #invoke-item "$archdir\$_"
-    #Start-sleep -s $PAUSE
+  # write-host "RAW: $_"
+  # write-host "WITHDIR: $archdir\$_"
+  Invoke-ShortcutIfExists $archdir\$_
+}
+
+# Run work specific progs
+if ( IsBusinessHours ) {
+  gci "$STARTUP_DIR/work/*" -Filter *.lnk -Exclude 00* | foreach-object {
+    Invoke-ShortcutIfExists $_
+  }
 }
--- setup.ps1
+++ setup.ps1
@@ -4,7 +4,7 @@ $DATE = Get-Date -Format yyyyMMdd-HHmmss
 get-item $profile | rename-item -newname { $_.Name -replace 'ps1',$curDateTime }
 
 # Install new profile
-Invoke-WebRequest -outfile $profile https://raw.githubusercontent.com/andylytical/powershell/master/Microsoft.PowerShell_profile.ps1
+Invoke-WebRequest -outfile $profile https://raw.githubusercontent.com/andylytical/powershell/main/Microsoft.PowerShell_profile.ps1
 
 ## Replace all files in PS1.d
 #$psub_dir = Join-Path -Path (get-item $profile).Directory.FullName -ChildPath 'PS1.d'
