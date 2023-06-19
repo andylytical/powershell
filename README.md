b59be8a7231416175d6c0749592eaa0c34420d11
Andy Loftus <aloftus@gmail.com>
2023-06-19 19:18:03 +0000

    refactor and add logging

--- README.md
+++ README.md
@@ -3,9 +3,14 @@
 # Installation
 - Powershell (as Administrator)
   - `Set-ExecutionPolicy RemoteSigned`
+- "Windows Powershell" (as Administrator) #see Notes below for more details
+  - `Set-ExecutionPolicy RemoteSigned`
 - Powershell (as normal user)
   - `get-item $profile | rename-item -newname { $_.Name -replace 'ps1',(Get-Date -Format yyyyMMdd-HHmmss) } -whatif`
   - `Invoke-WebRequest -outfile $profile https://raw.githubusercontent.com/andylytical/powershell/master/Microsoft.PowerShell_profile.ps1`
 
 # Powershell Tips / Tricks
 - [USERGUIDE.md](USERGUIDE.md)
+
+# Notes
+- Two different powershell installs ... https://superuser.com/questions/106360/how-to-enable-execution-of-powershell-scripts#comment2260136_106363
--- custom_startup.ps1
+++ custom_startup.ps1
@@ -1,68 +1,156 @@
-$STARTUP_DIR = "$env:USERPROFILE\Onedrive\Startup"
+###
+# SCRIPT VARIABLES
+###
+
+$STARTUP_DIR = [IO.Path]::Combine($env:USERPROFILE, 'Onedrive', 'Startup' )
+$DATETIME_STR = Get-Date -UFormat '%Y%m%d_%H%M%S_%Z'
+$LOGFILE = "${DATETIME_STR}_${env:COMPUTERNAME}.txt"
+$LOGPATH = [IO.Path]::Combine($STARTUP_DIR, 'logs', $LOGFILE)
+$INDENT_INCREMENT = 2
+$SH = New-Object -ComObject WScript.Shell # shell object needed for evaluating lnk target
 $PAUSE = 5
+New-Variable -Scope global -Name INDENT -Value '' #globally writeable
+
+# RUNTIME OPTIONS
+$VERBOSE = $true
 $DRYRUN = $false
 #$DRYRUN = $true
 
-$sh = New-Object -ComObject WScript.Shell
+
+###
+# FUNCTIONS
+###
+
+function Log {
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]$msg,
+
+    [Parameter()]
+    [string]$logfile = $LOGPATH
+  )
+
+  Add-Content -Path $logfile -Value $msg
+}
+
+
+function Info {
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]$msg
+  )
+
+  $newmsg = "[INFO] ${INDENT}${msg}"
+
+  Log $newmsg
+
+  if ($VERBOSE) {
+    Write-Host $newmsg
+  }
+}
+
+
+function Log-F-Enter {
+  $fname = (Get-Variable MyInvocation -Scope 1).Value.MyCommand.Name
+  Info "> ${fname}"
+  $global:INDENT += ' ' * $INDENT_INCREMENT
+}
+
+function Log-F-Exit {
+  $global:INDENT = $INDENT.Substring(0, $INDENT.Length - $INDENT_INCREMENT)
+  $fname = (Get-Variable MyInvocation -Scope 1).Value.MyCommand.Name
+  Info "< ${fname}"
+}
+
 
 function Invoke-ShortcutIfExists {
   param (
-    $raw_path
+    $lnk_path
   )
 
-  write-host "-----"
-  write-host "Invoke-ShortcutIfExists"
-  write-host $raw_path
-  $shortcut = $sh.CreateShortcut($raw_path)
+  Log-F-Enter
+  Info "lnk_path: '${lnk_path}'"
+  $shortcut = $SH.CreateShortcut($lnk_path)
   $tgt = $shortcut.TargetPath
-  write-host $tgt
+  Info "target: $tgt"
   $exists = Test-Path -Path $tgt
-  write-host "'$raw_path' -> '$tgt' ... exists? '$exists'"
-  if ( $exists -And -Not $DRYRUN ) { 
-    invoke-item "$raw_path"
-    Start-sleep -s $PAUSE
+  Info "target exists? $exists"
+  if ( $exists ) { 
+    if ( $DRYRUN ) {
+      Info "DRYRUN enabled, skipping '${lnk_path}'"
+    }
+    else {
+      invoke-item $lnk_path
+      Start-sleep -s $PAUSE
+    }
   }
+  Log-F-Exit
 }
 
+
 function IsBusinessHours {
+  Log-F-Enter
   $now = Get-Date
   $dayOfWeek = $now.DayOfWeek
   $hourOfDay = $now.Hour
-  write-host "DayOfWeek='$dayOfWeek'"
-  write-host "HourOfDay='$hourOfDay'"
+  Info "DayOfWeek: '$dayOfWeek'"
+  Info "HourOfDay: '$hourOfDay'"
+  $rv = $true
 
-  if ($dayOfWeek.ToString().StartsWith("S")) {
-    return $false
+  if ($dayOfWeek.ToString().StartsWith('S')) {
+    $rv = $false
+  }
+  elseif ($hourOfDay -lt 7 -or $hourOfDay -ge 17) {
+    $rv = $false
   }
 
-  if ($hourOfDay -lt 7 -or $hourOfDay -ge 17) {
-    return $false
-  }
-
-  write-host "IsBusinessHours = True"
-  return $true
+  Info "Return: ${rv}"
+  Log-F-Exit
+  return $rv
 }
 
-# Run all architecture agnostic progs
-gci "$STARTUP_DIR\*" -Filter *.lnk -Exclude 00* | foreach-object {
-  Invoke-ShortcutIfExists $_
-}
 
-# Run architecture specific progs
-# ...
-# see also (Get-WmiObject Win32_OperatingSystem).OSArchitecture
-# Returns: String ( "32-bit" OR "64-bit" )
-# https://blogs.msdn.microsoft.com/koteshb/2010/02/12/powershell-how-to-find-details-of-operating-system/
-$arch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture.Substring(0,2)
-$archdir = "$STARTUP_DIR\$arch"
-gci "$archdir" -Filter *.lnk | foreach { 
-  Invoke-ShortcutIfExists $archdir\$_
-}
+function Run-ShortcutsInDir {
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]$path
+  )
 
-# Run work specific progs
-if ( IsBusinessHours ) {
-  write-host "Looking for work shortcuts"
-  gci "$STARTUP_DIR\Work\*" -Filter *.lnk -Exclude 00* | foreach-object {
+  Log-F-Enter
+  Info "path = '${path}'"
+
+  # must have a "\*" at the end of path when using -Exclude with gci
+  $search_path = ( [IO.Path]::Combine( $path, '*' ) )
+  gci $search_path -File -Filter *.lnk -Exclude 00* | foreach-object {
     Invoke-ShortcutIfExists $_
+  }
+
+  Log-F-Exit
+}
+
+
+###
+# MAIN WORK SECTION
+###
+
+# Ensure log dir exists
+New-Item -ItemType Directory -Force -Path $(Split-Path -Path $LOGPATH -Parent)
+
+# Create a hash of "directories" and "tests"
+# Test should return a boolean; directory is included if "test" returns $true, otherwise it's skipped
+# Passing code reference via scriptblock, see: https://stackoverflow.com/a/53147390/21010651
+$dir_list = @{
+  Always = { $true }         # curly braces make a scriptblock
+  Work = { IsBusinessHours } # this function will be called when the scriptblock is invoked
+}
+
+$dir_list.GetEnumerator() | ForEach-Object {
+  $dir = ( [IO.Path]::Combine( $STARTUP_DIR, $_.Key ) )
+  # Use this search path only if the scriptblock returns True
+  if ( $_.Value.InvokeReturnAsIs() ) {
+    Run-ShortcutsInDir $dir
+  }
+  else {
+    Info "Negative test result: skipping directory ${dir}"
   }
 }
